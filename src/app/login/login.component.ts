02000b732b421eab901708ed01ee5e66458a046a
PierluigiPapa <pierluigipapa9@gmail.com>
2024-07-22 10:35:32 +0200

    fix: problem with Angular

---
+++ src/app/login/login.component.ts
@@ -0,0 +1,33 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent implements OnInit {
+
+  userID: string = "Pierluigi";
+  password: string = "";
+
+  autenticato: boolean = true;
+
+  errorMsg: string = "Spiacente, il nome utente e/o la password sono errati";
+
+  constructor(private route: Router) { }
+
+  ngOnInit(): void {
+  }
+
+  gestAuth = (): void => {
+    console.log(this.userID);
+
+    if (this.userID === "Pierluigi" && this.password === "Brindisi") {
+      this.route.navigate(['welcome', this.userID]);
+      this.autenticato = true;
+    } else {
+      this.autenticato = false;
+    }
+  }
+}
