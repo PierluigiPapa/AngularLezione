20a05b15757d1837ad0efc764881f85195be29cc
PierluigiPapa <pierluigipapa9@gmail.com>
2024-07-26 17:58:19 +0200

    feat: added cycle fort articles (lession 33)

--- src/app/components/articoli-card/articoli-card.component.ts
+++ src/app/components/articoli-card/articoli-card.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { IArticoli } from 'src/app/models/articoli';
+
 
 @Component({
   selector: 'app-articoli-card',
@@ -8,6 +10,19 @@ import { Component, OnInit } from '@angular/core';
 export class ArticoliCardComponent implements OnInit {
 
   constructor() { }
+
+  @Input()
+  articolo: IArticoli = {
+    codart: '',
+    descrizione: '',
+    um: '',
+    pzcart: 0,
+    peso: 0,
+    prezzo: 0,
+    active: true,
+    data: new Date (),
+    imageUrl: ''
+  };
 
   ngOnInit(): void {
   }
